cdbf1021a6cce238b6e165f9d160e5b334229cec
Juan Camilo Florez <jcflorezv@unal.edu.co>
2022-01-15 17:32:04 -0500

    choose swap surface format

--- trapp.cpp
+++ trapp.cpp
@@ -381,6 +381,17 @@ private:
         }
         return details;
     }
+
+    VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR> &availableFormats) {
+        for (const auto &availableFormat : availableFormats) {
+            if (availableFormat.format == VK_FORMAT_B8G8R8A8_SRGB && 
+                availableFormat.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR) {
+                    return availableFormat;
+                }
+        }
+
+        return availableFormats[0];
+    }
 };
 
 int main(int argc, char const *argv[]) {
