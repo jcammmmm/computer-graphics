3fac0841c1b270e2ea395f238e60d997f30db55c
Juan Camilo Florez <jcflorezv@unal.edu.co>
2022-01-15 17:55:42 -0500

    choose presentation mode

--- trapp.cpp
+++ trapp.cpp
@@ -383,14 +383,18 @@ private:
     }
 
     VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR> &availableFormats) {
-        for (const auto &availableFormat : availableFormats) {
+        for (const auto &availableFormat : availableFormats)
             if (availableFormat.format == VK_FORMAT_B8G8R8A8_SRGB && 
-                availableFormat.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR) {
+                availableFormat.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR)
                     return availableFormat;
-                }
-        }
-
         return availableFormats[0];
+    }
+
+    VkPresentModeKHR chooseSwapPresentMode(const std::vector<VkPresentModeKHR> &availablePresentModes) {
+        for (const auto &availablePresentMode : availablePresentModes) 
+            if (availablePresentMode == VK_PRESENT_MODE_MAILBOX_KHR)
+                return availablePresentMode;
+        return VK_PRESENT_MODE_FIFO_KHR;
     }
 };
 
